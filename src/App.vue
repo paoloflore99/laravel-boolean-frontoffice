49cea784c1d2072c4a4331a7c6ea6ecc1e69fb4a
stefanopriolo <stefanojpriolo@gmail.com>
2023-10-24 17:13:30 +0200

    Axios call + create pages

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <script>
-import TheHeader from './components/TheHeader.vue'
-import TheFooter from './components/TheFooter.vue'
-
-
 export default {
-  components: {
-    TheHeader,
-    TheFooter
-  }
+
 }
 </script>
 
 <template>
-  <TheHeader></TheHeader>
-  <main></main>
-  <TheFooter></TheFooter>
+  <router-view></router-view>
+
 </template>
 
 <style scoped></style>
