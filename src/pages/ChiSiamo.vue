60bfb7c91d968cc9fc0796da0d942e8609ff4fda
amani esseili <esseiliamani@hotmail.com>
2023-10-24 21:24:32 +0200

    add  pages

---
+++ src/pages/ChiSiamo.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+  <div class="container chi-siamo">
+    <div class="row">
+      <div class="col-6 custom-margin-top">
+        <img src="public/Cocktail-bar-chi-siamo.jpg" class="img-fluid" alt="immagine del bar">
+      </div>
+
+      <div class="col-6 custom-font custom-margin-top custom-bg ps-5 pe-5 pt-4">
+        <p>Benvenuti da Boolean, il bar che trasforma l'arte dei cocktail in una straordinaria esperienza sensoriale. <br>
+          La
+          nostra storia è nata dall'amore per l'innovazione e la creativita nel mondo dei drink. <br> il nostro obiettivo
+          e
+          offrire ai nostri clienti un viaggio unico tra sapori sorprendenti e una creazione di taleto e passione,
+          preparato con ingredienti freschi e techniche avanzate. <br> siamo qui per stupirvi e creare ricordi indimenticabile. <br>
+          unitevi a noi in questa avventura culinaria e scoprite il mondo di Boolean</p>
+
+      </div>
+    </div>
+
+  </div>
+</template>
+<style>
+body {
+  background-color: #404040;
+  margin: 0;
+  padding: 0;
+  color: white;
+}
+
+.custom-font {
+  font-size: 20px;
+}
+
+.custom-margin-top {
+  margin-top: 200px;
+}
+.custom-bg{
+  background-color:  #4C4C4C;
+}
+</style>
---
+++ src/pages/Contatti.vue
@@ -0,0 +1,70 @@
+
+<script>
+export default {
+
+}
+</script>
+<template>
+    <div class="container ">
+      <!-- descrizione pagina -->
+      <h1 class="text-center mt-5 mb-5">Reservation</h1>
+      <p class="text-center mt-5 mb-5 custom-font">Siamo sempre pronti a rispondere alle vostre domande e accogliere le vostre prenotazione. Non esitate a
+        contattarci.
+      </p>
+
+      <!-- form prenotazione -->
+
+      <form action="">
+        <div class="form-group custom-form text-center row">
+          <div class="col-sm-3">
+            <input type="number" class="form-control" placeholder="Numero di persone" v-model="numberOfPeople" />
+          </div>
+          <div class="col-sm-3">
+            <input type="date" class="form-control" v-model="reservationDate" />
+          </div>
+          <div class="col-sm-3">
+            <input type="time" class="form-control" v-model="reservationTime" />
+          </div>
+
+          <div class="col-sm-3">
+            <button class="btn custom-btn" @click="bookTable">Book a Table</button>
+          </div>
+        </div>
+
+      </form>
+
+      <!-- informazione contatti -->
+
+      <div class="contact-info d-flex justify-content-between">
+        <p><strong>Indirizzo:</strong> Via del Cocktail, 20</p>
+        <p><strong>Telefono:</strong> 0039 3452489551</p>
+        <p><strong>Email:</strong> info@booleancocktailbar.com</p>
+        <p><strong>Orari di Apertura:</strong> Lun - Ven 18:00 - 02:00, Sab - Dom 16:00 - 02:00</p>
+      </div>
+
+
+    </div>
+
+</template>
+<style>
+body{
+  background-color: #404040;
+  margin: 0;
+  padding: 0;
+  color: white;
+}
+
+.custom-font{
+  font-size: 20px;
+}
+
+.custom-btn{
+  background-color: #DEE756;
+}
+
+.custom-form{
+  margin-top: 200px;
+  margin-bottom: 300px;
+}
+
+</style>
