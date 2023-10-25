ff53847b331e4b6f883aeb86f292d094b3d5db95
amani esseili <esseiliamani@hotmail.com>
2023-10-25 09:27:34 +0200

    added logo and adjusted item spacing

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,30 +1,33 @@
 <script>
 export default {
-  
+
 
 
 }
 </script>
 <template>
   <header>
-    <nav class="navbar navbar-expand-lg bg-black">
+    
+    <nav class="navbar navbar-expand-lg bg-black justify-content-center">
+
       <div class="container-fluid">
-        <!-- Logo qui -->
+        <img class="ms-5 me-5" src="src/assets/img/logo-boolean.png" style="width: 120px;" alt="">
         <div class="navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <router-link class="nav-link active text-white" to="/" aria-current="page">Home</router-link>
-            <router-link class="nav-link text-white" to="/about">Chi Siamo</router-link>
-            <router-link class="nav-link text-white" to="/contacts">Contatti</router-link>
-            
+
+          <div class="navbar-nav ">
+            <router-link class="nav-link active text-white me-5" to="/" aria-current="page">Home</router-link>
+            <router-link class="nav-link text-white me-5" to="/about">Chi Siamo</router-link>
+            <router-link class="nav-link text-white me-5" to="/contacts">Contatti</router-link>
           </div>
         </div>
       </div>
+
     </nav>
     <div class="yellow-stripe"></div>
   </header>
 </template>
 <style>
-.yellow-stripe{
+.yellow-stripe {
   height: 5px;
   background-color: #A3A656;
   width: 100%;
